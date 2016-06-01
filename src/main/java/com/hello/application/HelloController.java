ac04409113c64696baf5fe55dd20d9a2396391aa
Alexandre Gama <alexandre.gama.lima@gmail.com>
2016-06-01 07:44:04 -0300

    Passing parameters throught request

--- src/main/java/com/hello/application/HelloController.java
+++ src/main/java/com/hello/application/HelloController.java
@@ -3,14 +3,17 @@ package com.hello.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @Controller
 public class HelloController {
 
 	@RequestMapping("/hello")
-	public String home() {
+	public String home(@RequestParam(name = "name", required = false, defaultValue = "Hello Spring") String message, Model model) {
+		model.addAttribute("message", message);
 		return "resultPage";
 	}
 
