65a4908f98fb2ef3b89661d376df17a84473b745
Alexandre Gama <alexandre.gama.lima@gmail.com>
2016-06-02 08:04:11 -0300

    Creates a new template default and Search Page to search terms on twitter

---
+++ src/main/java/com/tweets/application/SearchController.java
@@ -0,0 +1,14 @@
+package com.tweets.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SearchController {
+
+	@RequestMapping("/")
+	public String show() {
+		return "searchPage";
+	}
+	
+}
