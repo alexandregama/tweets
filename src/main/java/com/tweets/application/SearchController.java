43054f2190fe5a3fe74db4949061dc9cd33bbc0c
Alexandre Gama <alexandre.gama.lima@gmail.com>
2016-06-02 08:10:47 -0300

    Changes search page to send a post method

--- src/main/java/com/tweets/application/SearchController.java
+++ src/main/java/com/tweets/application/SearchController.java
@@ -1,7 +1,11 @@
 package com.tweets.application;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class SearchController {
@@ -9,6 +13,14 @@ public class SearchController {
 	@RequestMapping("/")
 	public String show() {
 		return "searchPage";
+	}
+	
+	@RequestMapping(value = "/search", method = RequestMethod.POST)
+	public String search(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+		String searchTerm = request.getParameter("forQuery");
+		redirectAttributes.addAttribute("forQuery", searchTerm);
+		
+		return "redirect:search-tweet";
 	}
 	
 }
