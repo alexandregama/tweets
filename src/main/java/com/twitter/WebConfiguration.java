d6c5eba84ceb16997af202d0801a58a8977d7aba
Alexandre Gama <alexandre.gama.lima@gmail.com>
2016-06-06 07:18:10 -0300

    Creates new Locale Interceptor to be used SessionLocale instead Default Locale

--- src/main/java/com/twitter/WebConfiguration.java
+++ src/main/java/com/twitter/WebConfiguration.java
@@ -3,8 +3,13 @@ package com.twitter;
 import java.time.LocalDate;
 
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import com.twitter.formatter.PersonalLocalDateFormatter;
 
@@ -16,4 +21,21 @@ public class WebConfiguration extends WebMvcAutoConfigurationAdapter {
 		registry.addFormatterForFieldType(LocalDate.class, new PersonalLocalDateFormatter());
 	}
 	
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new SessionLocaleResolver();
+	}
+	
+	// It will intercept any request made to Controller
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+		interceptor.setParamName("lang");
+		return interceptor;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
 }
